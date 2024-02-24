69580b79f92d40c759fc54224b20e41610904ac3
Joseph Attia <josephattia159@gmail.com>
2024-02-24 10:53:00 -0500

    baby steps

---
+++ backend/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from flask import url_for
+from flask_cors import CORS
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+italy = {
+    "name": "Italy",
+    "population": 60_000_000,
+    "capital": "Rome",
+    "language": "Italian",
+    "currency": "Euro",
+}
+
+# api endpoint that add flight cost the databse for each country dynamically
+ 
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.get("/api/places")
+def get_places():
+    return {"places": ["place1", "place2"]}
+
+# /reg/user/<id>
+
+@app.get("/api/places/<place_id>")
+def get_place(place_id):
+    # for:
+    #     if place_id == "italy":
+            # call a request to google flights api passing italy as the destination and the user location as the origin and save the cost in the database
+    
+    if place_id == "italy":
+        return italy
+    return {"place": place_id}
+
+if __name__ == '__main__':
+    app.run(debug=True)
